14fd8a9a5128cfa3bd930b93ec08d1ede0629f47
Baptiste HULIN <batimon77@gmail.com>
2019-01-30 20:53:22 +0100

    Scrapping at MagicCorpDone : nameVo&VF, image, rarity, color -- TOFINISH : cost

--- 2Scrapy/tutorial/tutorial/spiders/__init__.py
+++ 2Scrapy/tutorial/tutorial/spiders/__init__.py
@@ -10,13 +10,42 @@ class MagicCorpSpider(scrapy.Spider):
 
     def parse(self, response):
         for card in response.xpath("//tr[@class='hover']"):
-            name = card.xpath("string(td[4]/span/a/@title)").extract()
+            nameVO = card.xpath("string(td[4]/span/a/@title)").extract()
+            nameVF = card.xpath("string(td[5]/span/a/@title)").extract()
             image = card.xpath("td[1]/a/img/@src").extract()
+            rarity = card.xpath("td[2]/img/@src").extract()
+            color = card.xpath("td[6]/img[last()]/@src").extract()
 
-            for item in zip(name,image):
+
+            if(rarity == ['/images/magic/rarete/normal/mystic_rare.gif']):
+                rarity = ["mythic"]
+            elif(rarity == ['/images/magic/rarete/normal/uncommon.gif']):
+                rarity = ["uncommon"]
+            elif(rarity == ['/images/magic/rarete/normal/common.gif']):
+                rarity = ["common"]
+            elif(rarity == ['/images/magic/rarete/normal/rare.gif']):
+                rarity = ['rare']
+
+            if(color == ['/images/magic/manas/mini/w.gif']):
+                color = ['white']
+            elif(color == ['/images/magic/manas/mini/u.gif']):
+                color = ['blue']
+            elif(color == ['/images/magic/manas/mini/b.gif']):
+                color = ['black']
+            elif(color == ['/images/magic/manas/mini/r.gif']):
+                color = ['red']
+            elif(color == ['/images/magic/manas/mini/g.gif']):
+                color = ['green']
+            else:
+                color = ['uncolor']
+
+            for item in zip(nameVO, nameVF ,image, rarity, color):
                 scraped_info = {
-                'name' : item[0],
-                'image' : item[1]
+                'nameVO' : item[0],
+                'nameVF' : item[1],
+                'image' : item[2],
+                'rarity' : item[3],
+                'color' : item[4]
                 }
 
                 yield scraped_info
